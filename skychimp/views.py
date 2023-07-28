a7b039123eef36c488ad95e8e2950e0098c82bd0
gau <gau@1lz.ru>
2023-07-29 00:28:09 +0300

    Dev refactoring blog

--- skychimp/views.py
+++ skychimp/views.py
@@ -7,19 +7,11 @@ from blog.models import Blog
 
 
 def main_view(request):
-    # products_list = Product.objects.all()
-    blog_3_post = random.sample(list(Blog.objects.filter(is_published=True)), 3)
     context = {
-        # 'object_list': products_list,
-        'title': 'Главная страница',
-        'blogs': blog_3_post
+        'title': 'Главная страница'
     }
-
-
+    count_blog = len(Blog.objects.filter(is_published=True))
+    if count_blog > 0:
+        blog_3_post = random.sample(list(Blog.objects.filter(is_published=True)), count_blog if count_blog < 3 else 3)
+        context['blogs'] = blog_3_post
     return render(request, 'skychimp/main.html', context)
-
-
-# class MainView(FormView):
-#     template_name = 'skychimp/main.html'
-
-
